b712786baa8d4238d8f828164cee73efe2a968bc
Álvaro Muñoz Delgado <ingenierolibre@gmail.com>
2018-11-21 23:42:01 +0800

    No more Bcrypt

--- src/main/java/ee4216/backend/config/SecurityConfig.java
+++ src/main/java/ee4216/backend/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
             
             auth.jdbcAuthentication().dataSource(dataSource)
-                    .passwordEncoder(new BCryptPasswordEncoder())
+                    //.passwordEncoder(new BCryptPasswordEncoder())
                     .usersByUsernameQuery("select username,password, enabled from users where username=?")
                     .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
